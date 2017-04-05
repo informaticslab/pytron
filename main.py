f78f30cb2c868c04c91f37513a272a2ce6052006
Greg Ledbetter <gsledbetter@gmail.com>
2017-04-05 16:27:22 -0400

    Get TV status at launch and set power, input source and volume accordingly.

--- main.py
+++ main.py
@@ -17,6 +17,10 @@ from time import sleep
 dst_ip = '192.168.0.10'       # ip of tv
 dst_port = 1515
 
+init_power = 0
+init_source = 0x21
+init_volume = 25
+
 mdc_format = Struct(
     "fields" / RawCopy(Struct(
         "header" / Const(Int8ub, 0xAA),
@@ -28,13 +32,40 @@ mdc_format = Struct(
     "checksum" / Checksum(Bytes(1), lambda data: sum(bytearray(data)[1:]) % 256, this.fields.data),
 )
 
+mdc_status_request = Struct(
+    "fields" / RawCopy(Struct(
+        "header" / Const(Int8ub, 0xAA),
+        "cmd" / Const(Int8ub, 0x00),
+        "id" / Const(Int8ub, 0x01),
+        "msg_length" / Const(Int8ub, 0x00),
+    )),
+    "checksum" / Checksum(Bytes(1), lambda data: sum(bytearray(data)[1:]) % 256, this.fields.data),
+)
+mdc_status_response = Struct(
+    "fields" / RawCopy(Struct(
+        "header" / Const(Int8ub, 0xAA),
+        "cmd" / Int8ub,
+        "id" / Const(Int8ub, 0x01),
+        "msg_length" / Int8ub,
+        "ack_nack" / Int8ub,
+        "rcmd" /Int8ub,
+        "power" / Int8ub,
+        "volume" / Int8ub,
+        "mute" / Int8ub,
+        "input" / Int8ub,
+        "aspect" / Int8ub,
+        "ntime" / Int8ub,
+        "ftime" / Int8ub,
+    )),
+    "checksum" / Checksum(Bytes(1), lambda data: (sum(bytes(data)[1:]) % 256).to_bytes(1, byteorder='big'), this.fields.data),
+)
+
 
 def send_mdc_msg(msg):
 
-    return
     # create socket
-    #print('# Creating socket')
-    #print("Sending MDC Msg {}".format(hexlify(msg)))
+    print('# Creating socket')
+    print("Sending MDC Msg {}".format(hexlify(msg)))
 
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -52,11 +83,55 @@ def send_mdc_msg(msg):
 
 
     # Receive data
-    #print('# Receive data from server')
-    reply = sock.recv(1024)
+    print('# Receive data from TV')
+    #reply = sock.recv(1024)
     #print(reply)
 
     sock.close()
+
+
+def get_tv_status():
+    global init_power, init_source, init_volume
+
+    data = mdc_status_request.build(dict(fields=dict(value=dict())))
+    print("Sending Status Request packet = {}".format(hexlify(data)))
+
+    # create socket
+    print('# Creating socket')
+    print("Sending MDC Msg {}".format(hexlify(data)))
+
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    except socket.error:
+        print('Failed to create socket')
+        sys.exit()
+
+    sock.connect((dst_ip, dst_port))
+
+    try:
+        sock.sendall(data)
+    except socket.error:
+        print('Send failed')
+        sys.exit()
+
+    # Receiving from client
+    data = sock.recv(1024)
+
+    print("Received packet = {}".format(hexlify(data)))
+    #print("Checksum = {}".format(hexlify((sum(bytearray(data)[1:-1])).to_bytes(1, byteorder='big'))))
+
+    resp = mdc_status_response.parse(data)
+    print("Parsed message = {}".format(resp))
+    print("ACK/NACK = {}".format(resp.fields.value.ack_nack))
+
+    init_power = resp.fields.value.power
+    init_source = resp.fields.value.input
+    init_volume = resp.fields.value.volume
+
+
+    # came out of loop
+    sock.close()
+
 
 class TvControlScreen(GridLayout):
 
@@ -97,19 +172,6 @@ class TvControlScreen(GridLayout):
         self.add_widget(self.btnSourceDp3)
 
 
-class ToggleButton(ToggleButtonBehavior, Image):
-    def __init__(self, **kwargs):
-        super(MyButton, self).__init__(**kwargs)
-        self.source = 'atlas://data/images/defaulttheme/checkbox_off'
-
-    def on_state(self, widget, value):
-        if value == 'down':
-            self.source = 'atlas://data/images/defaulttheme/checkbox_on'
-        else:
-            self.source = 'atlas://data/images/defaulttheme/checkbox_off'
-
-
-
 class PowerLayout(Widget):
     def __init__(self, **kwargs):
         super(PowerLayout, self).__init__(**kwargs)
@@ -136,6 +198,20 @@ class HdmiLayout(Widget):
 class RootContainer(FloatLayout):
     def __init__(self, **kwargs):
         super(RootContainer, self).__init__(**kwargs)
+        if init_power == 0:
+            self.ids.powerOff.state = 'down'
+        else:
+            self.ids.powerOn.state = 'down'
+
+        if init_source == 0x21 or init_source == 0x22:
+            self.ids.hdmi1.state = 'down'
+        elif init_source == 0x23 or init_source == 0x24:
+            self.ids.hdmi2.state = 'down'
+        else:
+            self.ids.hdmi1.state = 'normal'
+            self.ids.hdmi2.state = 'normal'
+
+        self.ids.volume.value = init_volume
 
     def btn_power_on_touched(self):
         data = mdc_format.build(dict(fields=dict(value=dict(cmd=0x11, msg_length=0x01, msg_data=0x01))))
@@ -161,7 +237,7 @@ class RootContainer(FloatLayout):
         self.ids.hdmi2.state = 'down'
         send_mdc_msg(data)
 
-    def set_volume(self, value):
+    def set_volume(self, instance, value):
         print("Volume value = {}".format(value))
         vol = int(value)
         data = mdc_format.build(dict(fields=dict(value=dict(cmd=0x12, msg_length=0x01, msg_data=vol))))
@@ -178,9 +254,7 @@ class PyTronApp(App):
 
 
 def init():
-    data = mdc_format.build(dict(fields=dict(value=dict(cmd=0x12, msg_length=0x01, msg_data=25))))
-    send_mdc_msg(data)
-
+    get_tv_status()
     sleep(0.2)
 
 if __name__ == '__main__':
